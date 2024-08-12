4ab706583c2558255a01510945ac4da2d6d7546e
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-08-12 19:48:11 +0100

    Filled in all the functions so far.
    Now I just need to fix up my menu...

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@ vector<Customer> allCustomers;
 
 void addCustomer();
 void displayCustomers();
-void findCustomer();
-void findByPurchase();
-void manageData();
+const Customer* findCustomer(int customerId);
+const Customer* findByPurchase(int purchaseNumber);
+void saveCustomerData(const std::vector<Customer>& customers);
+void loadCustomerData();
 
 int main() {
     std::cout << "Customer Management System" << std::endl;
-    return 0;
-
     int menu;
     do {
         std::cout
@@ -33,6 +32,7 @@ int main() {
                 addCustomer();
                 break;
             case 2:
+                displayCustomers();
                 break;
             case 3:
                 break;
@@ -48,6 +48,7 @@ int main() {
         }
     } while (menu > 0 && menu <= 8);
 
+    return 0;
 }
 
 void addCustomer() {
@@ -100,7 +101,7 @@ void addCustomer() {
             cout << "Invalid." << endl;
         }
     }
-    Customer custom = *new Customer(id, title, name, purchaseCount, purchase[purchaseCount], type); // creation of an object
+    Customer custom(id, name, title, purchaseCount, purchases, type); // creation of an object
 
     allCustomers.push_back(custom);
 }
@@ -113,14 +114,61 @@ void displayCustomers() {
     }
 }
 
-void findCustomer() {
-
+const Customer* findCustomer(int customerId) {
+    for (int i = 0; i < allCustomers.size(); i++) {
+        if (allCustomers[i].getCustomerId() == customerId) {
+            return &allCustomers[i];
+        }
+    }
+    return nullptr; // Customer not found
 }
 
-void findByPurchase() {
-
+const Customer* findByPurchase(int *purchaseNumber) {
+    for (int i = 0; i < allCustomers.size(); i++) {
+        for (int j = 0; j < allCustomers[i].getPurchaseCount(); j++) {
+            if (allCustomers[i].getPurchase() == purchaseNumber) {
+                return &allCustomers[i];
+            }
+        }
+    }
+    return nullptr; // Customer not found
 }
 
-void manageData() {
-
+// Function to save customer data to a text file
+void saveCustomerData(const std::vector<Customer>& customers) {
+    std::ofstream file("customers.txt");
+    if (file.is_open()) {
+        for (const Customer& customer : customers) {
+            file << customer.getCustomerId() << " " << customer.getTitle() << " " << customer.getName() << " " << customer.getPurchaseCount() << " " << customer.getType() << std::endl;
+            for (int i = 0; i < customer.getPurchaseCount(); i++) {
+                file << customer.getPurchase() << " ";
+            }
+            file << std::endl;
+        }
+        file.close();
+        std::cout << "Customer data saved to customers.txt" << std::endl;
+    } else {
+        std::cerr << "Unable to open file for writing" << std::endl;
+    }
+}
+// Function to load customer data from a text file
+void loadCustomerData() {
+    std::ifstream file("customers.txt");
+    vector<Customer> customers;
+    if (file.is_open()) {
+        int id, purchaseCount;
+        std::string title, name, type;
+        while (file >> id >> title >> name >> purchaseCount >> type) {
+            int purchases[purchaseCount];
+            for (int i = 0; i < purchaseCount; i++) {
+                file >> purchases[i];
+            }
+            Customer c(id, name, title, purchaseCount, purchases, type);
+            customers.push_back(c);
+        }
+        file.close();
+        std::cout << "Customer data loaded from customers.txt" << std::endl;
+    } else {
+        std::cerr << "Unable to open file for reading" << std::endl;
+    }
 }
